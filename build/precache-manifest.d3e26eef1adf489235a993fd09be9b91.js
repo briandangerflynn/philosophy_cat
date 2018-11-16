9a00155cedc09f84ccf268b3d9371b353a62d044
briandangerflynn <bdflynny@gmail.com>
2018-11-16 13:15:21 -0500

    first commit

---
+++ build/precache-manifest.d3e26eef1adf489235a993fd09be9b91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bf6df18f3538f54ef8b",
+    "url": "/philosophy_cat/static/js/runtime~main.1bf6df18.js"
+  },
+  {
+    "revision": "a503b7b17e35e87c9bf4",
+    "url": "/philosophy_cat/static/js/main.a503b7b1.chunk.js"
+  },
+  {
+    "revision": "09689009d67fed834754",
+    "url": "/philosophy_cat/static/js/1.09689009.chunk.js"
+  },
+  {
+    "revision": "a503b7b17e35e87c9bf4",
+    "url": "/philosophy_cat/static/css/main.29587d13.chunk.css"
+  },
+  {
+    "revision": "901aba81009199891238ae23ca8e4ae0",
+    "url": "/philosophy_cat/index.html"
+  }
+];
---
+++ src/index.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Main extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      quote: "There's only one thing I hate more than lying: skim milk. Which is water that's lying about being milk."
+    };
+  }
+
+
+
+  getQuote() {
+    const apiKey = "IbS_pi4yPKyL7suY6RADFAeF";
+    const url = "http://quotes.rest/quote/random";
+
+    this.setState({
+      quote: "Thinking purrr..."
+    })
+
+    fetch(url, {
+      headers: {
+        "Accept": "application/json",
+        "X-TheySaidSo-Api-Secret": apiKey
+      },
+    })
+    .then((res) => {
+      return res.json();
+    })
+    .then((myJson) => {
+      const quote = JSON.stringify(myJson.contents.quote).replace(/['"]+/g, '')
+      this.setState({
+        quote: quote,
+      })
+    })
+  }
+
+
+
+  render () {
+    const quote = this.state.quote
+    return (
+      <div>
+        <header>
+          <h1>Philosophy Cat Says:</h1>
+        </header>
+        <main>
+          <section id="cat-image">
+            <img src="https://t1.ea.ltmcdn.com/en/images/2/0/5/img_my_cat_is_depressed_1502_paso_0_600.jpg"/>
+          </section>
+          <section id="cat-text">
+            <div id="quote-box">
+              <p id="quote-text">{quote}</p>
+            </div>
+            <button id="next-quote" onClick={() => this.getQuote()}>Next Quote</button>
+          </section>
+        </main>
+        <footer>
+          <p>Created by Brian Danger Flynn &mdash; 2018</p>
+        </footer>
+      </div>
+    );
+  }
+}
+
+
+ReactDOM.render(
+  <Main />,
+  document.getElementById('root')
+);
+
+
